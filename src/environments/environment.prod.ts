0d34468677ed451240789416c0a5ef6aae40c7f1
Josué Casasola <jcasasola@musicbattles.mx>
2022-08-24 13:12:02 -0500

    feat: add firebaseconfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  url: 'https://leasemd-dev.azure-api.net/front-test/'
+  url: 'https://leasemd-dev.azure-api.net/front-test/',
+  firebaseConfig: {
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
+  }
 };
